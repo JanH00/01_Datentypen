5ec03ab8ab195234a67e96e0395c2c3b4da09817
JanH00 <67894980+JanH00@users.noreply.github.com>
2020-07-07 11:50:30 +0200

    Create 01_Dartentype.dart

---
+++ 01_Dartentype.dart
@@ -0,0 +1,55 @@
+/*
+
+void main() {
+  int zahl = 2;
+  double kommerzahl = 1.5;
+  String a = "Test";
+  bool Wahr = true;
+  
+  print(zahl + kommerzahl);
+  print(a);
+  
+  if(Wahr) {
+    print(a + " Hey");
+  }
+}
+
+*/
+
+void main() {
+  
+  //Aufgabe 1
+  
+  int zahl1 = 2;
+  int zahl2 = 5;
+  
+  print(zahl1);
+  print(zahl2);
+  
+  //Aufgabe 2
+  
+  double kommerzahl1 = 2.5;
+  double kommerzahl2 = 3.5;
+  
+  print(kommerzahl1);
+  print(kommerzahl2);
+  
+  //Aufgabe 3
+  
+  String a = "Test1";
+  String b = "Test2";
+  
+  print(a);
+  print(b);
+
+}
+
+
+
+
+
+
+
+
+
+
